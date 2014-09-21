b1ff3c73d5aa9896e1da9d11d681bae01ba76f3e
shivakomatreddy <shivakomatreddy@gmail.com>
2014-09-21 11:18:21 -0400

    Backed Up Mongo Implementation Of Routes

---
+++ goodeggs/routes/mongodbRoutesImplementation/market.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/productsList/:type',function(req,res){
+  var db = req.db;
+  var productType = req.params.type;
+  db.collection('products').find({type:productType}).toArray(function (err,items){
+    res.json(items)
+  });
+});
+
+
+
+// Get Products From Product's List
+router.get('/productsList',function(req,res) {
+  	var db = req.db;
+    var productType = req.params.type;
+    db.collection('products').find().toArray(function (err,items) {
+  		  res.json(items);
+    });
+});
+
+
+
+module.exports = router;
